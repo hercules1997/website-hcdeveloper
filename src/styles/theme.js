29dfc74bcfc8286541d14d97e27b58c001eae042
hercules1997 <hercules.chaves.andrade@gmail.com>
2024-04-29 23:38:50 -0300

    Aplicando tema de cores

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primary: "#4E4E4E",
+    success: "#00ed64",
+    selected: "#1F03D5",
+    warning: "#ffc010",
+    error: "#db3030",
+    white: "#ffffff",
+    light: "#e8edeb",
+    neutral: "#c1c7c6",
+    dark: "#001e2b",
+    black: "#000000",
+  },
+};
