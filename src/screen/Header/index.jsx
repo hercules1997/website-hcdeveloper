8e2d67f13973625144c65f9833e5a6ed85697f90
hercules1997 <hercules.chaves.andrade@gmail.com>
2024-05-08 21:23:07 -0300

    up

--- src/screen/Header/index.jsx
+++ src/screen/Header/index.jsx
@@ -39,7 +39,6 @@ export function Header() {
           <div>
             <TextApresentation>
               <span>
-                {' '}
                 Esta precisando de um especialista em desenvolvimento?
               </span>
             </TextApresentation>
